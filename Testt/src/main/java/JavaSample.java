d31d7b6ca7759624fc6003765457fd449687ff6d
HP <HP@DESKTOP-35UH0L5>
2024-01-03 12:59:51 +0530

    commit

---
+++ Testt/src/main/java/JavaSample.java
@@ -0,0 +1,12 @@
+
+public class JavaSample {
+	static int f=5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=a+f;
+System.out.println("sum"+b);
+	}
+
+}
