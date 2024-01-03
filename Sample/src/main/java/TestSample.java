7480599eca91a7076358f5e91ecd6baaeaa6ebdc
HP <HP@DESKTOP-35UH0L5>
2024-01-03 12:45:29 +0530

    first commit

---
+++ Sample/src/main/java/TestSample.java
@@ -0,0 +1,19 @@
+
+public class TestSample {
+	public void hello()
+	{
+		System.out.println("Hellooo");
+	}
+		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestSample S=new TestSample();
+		S.hello();
+			}
+
+	}
+
+
+
+
